d4e15e8255f756b386f0ffc23944d64dec3bad6a
leeool <lgonsalez02@gmail.com>
2022-03-18 22:33:07 -0300

    finaliza

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿string frase;
+
+Console.Write("Digite uma frase: ");
+frase = Console.ReadLine()!;
+
+frase = frase   
+    .Replace("a", "4")
+    .Replace("A", "4")
+    .Replace("i", "1")
+    .Replace("I", "1")
+    .Replace("s", "5")
+    .Replace("S", "5")
+    .Replace("t", "7")
+    .Replace("T", "7")
+    .Replace("e", "3")
+    .Replace("E", "3")
+    .Replace("o", "0")
+    .Replace("O", "0");
+
+Console.WriteLine($"{frase}");
